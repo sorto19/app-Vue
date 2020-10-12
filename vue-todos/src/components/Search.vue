96353afaacc5ea7f874a7181c5b9ed5b9edce160
Ramon sorto <ramonsorto1@gmail.com>
2020-10-12 16:28:59 -0600

    falta una lib

---
+++ vue-todos/src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+ <div id="search-container">
+ <input type="text" v-model="query" v-on:input="$emit('query-change', query)">
+</div>    
+</template>
+<script>
+export default {
+    name: 'Search',
+    data(){
+        return {
+            query: ''
+        }
+    }
+}
+</script>
+<style  scoped>
+
+
+</style>
